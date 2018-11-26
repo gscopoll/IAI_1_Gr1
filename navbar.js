911c7621b9c7acb9f9d91d96c358e4a6b0e54acb
Konrad Semik <konradsemik@gmail.com>
2018-11-26 17:21:22 +0100

    kol

---
+++ navbar.js
@@ -0,0 +1,2 @@
+let nav = '<h1 href="index.html"></h1>' +
+    '<h1 href="kontakt.html"></h1>';
---
+++ script.js
@@ -0,0 +1,7 @@
+let przycisk=get.getElementById("przycisk");
+let przycisk.addEventListener("click", zmienkolor);
+let okienko=get.getElementById("okienko");
+function zmienkolor {
+    okienko.className= "czerwony";
+}
+
